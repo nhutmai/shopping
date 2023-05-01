2ed11b1b08fcb59d12ec61383cd72ba685293287
nhutmai <nhutmai002@gmail.com>
2023-05-02 02:44:30 +0800

    123

--- src/App.js
+++ src/App.js
@@ -2,24 +2,13 @@ import "./App.css";
 import Footer from "./components/footer/footer";
 import Header from "./components/header/header";
 import ProductList from "./components/productlist/productlist";
-import Product from "./API/getproduct";
-import { useEffect, useState } from "react";
-import getproduct from "./API/getproduct";
 import { Route, Routes } from "react-router-dom";
 import about from "./components/about/about";
-import product from "./components/products/product";
 import cart from "./components/cart/cart";
 import Login from "./page/login/login";
 import Homepage from "./components/home/home";
 
 function App() {
-  useEffect(() => {
-    const fetchProduct = async () => {
-      const product = await getproduct.getAll();
-      console.log(product);
-    };
-    fetchProduct();
-  }, []);
   return (
     <div className="App">
       <Header />
